741f6b453ab5d10488387d60ddc2c8e8f3061383
Abdul <muiz.rpi@gmail.com>
2020-10-08 11:22:48 -0400

    fixed property & review schema:

--- server/schemas/review.schema.ts
+++ server/schemas/review.schema.ts
@@ -4,11 +4,17 @@ var Schema = mongoose.Schema;
 
 //This needs to be fine tuned to match our schema specs.
 var ReviewSchema = new Schema({
-    property_id: String,
-    student_id: String,
+    property_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Property'
+    },
+    student_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Student'
+    },
     content: String,
     rating: Number, //1-5? idk how this is gonna work
-    term: Date
+    term: Object
 });
 
 const Review = mongoose.model("Review", ReviewSchema);
