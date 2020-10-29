889e1fe5a7b7ff3526d7dcce91a8317cbbd20d96
Abdul <muiz.rpi@gmail.com>
2020-10-29 07:01:14 -0400

    Added route restrictions by access permission level

--- web/src/index.js
+++ web/src/index.js
@@ -65,17 +65,19 @@ const Routes = () => {
         <Switch>
 
           {/* Unrestricted Paths */}
-          <Route exact path="/" component={LandingView} />
-          <Route exact path="/student/auth-cas" component={StudentCASAuth} />
-          <Route exact path="/landlord/login" component={LandlordLoginView} />
-          <Route exact path="/student/login" component={StudentLoginView} />
-          <Route exact path="/landlord/register" component={LandlordRegisterView} />
+          <AuthRoute accessLevel={AccessLevels.UNAUTH} exact path="/" component={LandingView} />
+          <AuthRoute accessLevel={AccessLevels.UNAUTH} exact path="/student/auth-cas" component={StudentCASAuth} />
+          <AuthRoute accessLevel={AccessLevels.UNAUTH} exact path="/landlord/login" component={LandlordLoginView} />
+          <AuthRoute accessLevel={AccessLevels.UNAUTH} exact path="/student/login" component={StudentLoginView} />
+          <AuthRoute accessLevel={AccessLevels.UNAUTH} exact path="/landlord/register" component={LandlordRegisterView} />
 
           
           {/* Restricted Paths */}
           <AuthRoute accessLevel={AccessLevels.STUDENT} exact path="/student/register/complete" component={StudentRegisterComplete} />
           <AuthRoute accessLevel={AccessLevels.STUDENT_AND_LANDLORD} exact path="/property/:id" component={({match}) => (<PropertyView property_id={match.params.id} />)} />
           <AuthRoute accessLevel={AccessLevels.STUDENT} exact path="/search" component={SearchView} />
+          
+          {/* 404 */}
           <Route component={NotFound} />
         </Switch>
     </AlertContext.Provider>
--- web/src/modules/auth/AuthRoute.tsx
+++ web/src/modules/auth/AuthRoute.tsx
@@ -27,6 +27,7 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
   const user = useSelector((state: any) => state.user)
 
   useEffect(() => {
+
     // get the user if the user does not exist
     dispatch(fetchUser(user, {update: false}))
 
@@ -45,9 +46,26 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
   }, [user])
 
   return (<Route {...rest} render={(props) => {
-    if (auth.loaded) {
-      if (auth.isAuthenticated) return <Component {...props} />
-      else return <Redirect to="/" />
+
+    // if this route is set for any user level (no restriction)
+    if (accessLevel == AccessLevels.ANY) return <Component {...props} />
+
+    else if (auth.loaded) {
+      // If I am authenticated, I can access components with accessLevel of authenticated user
+      if (auth.isAuthenticated) {
+
+        if (accessLevel == AccessLevels.UNAUTH) return <Redirect to="/home" />
+        else if (accessLevel == AccessLevels.STUDENT
+          || accessLevel == AccessLevels.LANDLORD
+          || accessLevel == AccessLevels.STUDENT_AND_LANDLORD) return <Component {...props} />
+
+      }
+      else {
+
+        if (accessLevel == AccessLevels.UNAUTH) return <Component {...props} />
+        else return <Redirect to="/" />
+
+      }
     }
     else return <div />
     
