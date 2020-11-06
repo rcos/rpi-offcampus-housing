f2535933591c7681867c7ab67b7690bc5aaec5f2
Abdul <muiz.rpi@gmail.com>
2020-11-05 23:04:30 -0500

    fixed warnings in AuthRouter.tsx

--- web/src/modules/auth/AuthRoute.tsx
+++ web/src/modules/auth/AuthRoute.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import {Route, Redirect} from 'react-router'
 import AccessLevels from './accessLevels.json'
-import AuthAPI from '../../API/AuthAPI'
+// import AuthAPI from '../../API/AuthAPI'
 
 import _ from 'lodash'
 
 // redux
-import {getUser, fetchUser} from '../../redux/actions/user'
+import {fetchUser} from '../../redux/actions/user'
 import {useDispatch, useSelector} from 'react-redux'
 
 interface IAuthStatus {
@@ -19,17 +19,17 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
 
   const getUserType = (user_type: any): number => {
     // auth.user != undefined && _.has(auth.user, 'type') && (auth.user as any).type == "landlord"
-    if (auth.user == undefined) return AccessLevels.UNAUTH
+    if (auth.user === undefined) return AccessLevels.UNAUTH
     else if (_.has(auth.user, 'type')) {
-      if ((auth.user as any).type == "landlord") return AccessLevels.LANDLORD
-      if ((auth.user as any).type == "student") return AccessLevels.STUDENT
+      if ((auth.user as any).type === "landlord") return AccessLevels.LANDLORD
+      if ((auth.user as any).type === "student") return AccessLevels.STUDENT
     }
     return AccessLevels.UNAUTH
   }
   const defaultRoute = (user_type: number): string => {
-    if (user_type == AccessLevels.STUDENT) return '/search'
-    if (user_type == AccessLevels.LANDLORD) return '/landlord/dashboard'
-    if (user_type == AccessLevels.UNAUTH) return '/'
+    if (user_type === AccessLevels.STUDENT) return '/search'
+    if (user_type === AccessLevels.LANDLORD) return '/landlord/dashboard'
+    if (user_type === AccessLevels.UNAUTH) return '/'
     return '/'
   }
 
@@ -47,7 +47,7 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
     // get the user if the user does not exist
     dispatch(fetchUser(user, {update: false}))
 
-  }, [])
+  }, [dispatch, user])
 
   useEffect(() => {
 
@@ -65,35 +65,35 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
   return (<Route {...rest} render={(props) => {
 
     // if this route is set for any user level (no restriction)
-    if (accessLevel == AccessLevels.ANY) return <Component {...props} />
+    if (accessLevel === AccessLevels.ANY) return <Component {...props} />
 
     else if (auth.loaded) {
       // If I am authenticated, I can access components with accessLevel of authenticated user
       if (auth.isAuthenticated) {
 
-        if (accessLevel == AccessLevels.UNAUTH) return <Redirect to={defaultRoute(getUserType(auth))} />
+        if (accessLevel === AccessLevels.UNAUTH) return <Redirect to={defaultRoute(getUserType(auth))} />
 
         /*
         If we try to access a route only allowed to students, as a student, allow it.
         If we are not a student, redirect us to the landlord route
         */
-        else if (accessLevel == AccessLevels.STUDENT) {
-          if (auth.user != undefined && _.has(auth.user, 'type') && (auth.user as any).type == "student") {
+        else if (accessLevel === AccessLevels.STUDENT) {
+          if (auth.user !== undefined && _.has(auth.user, 'type') && (auth.user as any).type === "student") {
             return <Component {...props} />
           }
           else return <Redirect to={defaultRoute(getUserType(auth))} />
         }
 
-        else if (accessLevel == AccessLevels.LANDLORD) {
-          if (auth.user != undefined && _.has(auth.user, 'type') && (auth.user as any).type == "landlord") {
+        else if (accessLevel === AccessLevels.LANDLORD) {
+          if (auth.user !== undefined && _.has(auth.user, 'type') && (auth.user as any).type === "landlord") {
             return <Component {...props} />
           }
           else return <Redirect to={defaultRoute(getUserType(auth))} />
         }
 
-        else if (accessLevel == AccessLevels.STUDENT_AND_LANDLORD) {
-          if (auth.user != undefined && _.has(auth.user, 'type') && 
-          ((auth.user as any).type == "landlord" || (auth.user as any).type == "student")) {
+        else if (accessLevel === AccessLevels.STUDENT_AND_LANDLORD) {
+          if (auth.user !== undefined && _.has(auth.user, 'type') && 
+          ((auth.user as any).type === "landlord" || (auth.user as any).type === "student")) {
             return <Component {...props} />
           }
           else return <Redirect to={defaultRoute(getUserType(auth))} />
@@ -105,7 +105,7 @@ const AuthRoute = ({component: Component, accessLevel, ...rest}: any) => {
       }
       else {
 
-        if (accessLevel == AccessLevels.UNAUTH) return <Component {...props} />
+        if (accessLevel === AccessLevels.UNAUTH) return <Component {...props} />
         else return <Redirect to={defaultRoute(getUserType(auth))} />
 
       }
