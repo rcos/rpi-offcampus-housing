e13eb1d4847b5cf0111d2a1052605042fa90f85c
Abdul <muiz.rpi@gmail.com>
2020-11-07 23:53:50 -0500

    Added smoother animation to minimize component

--- web/src/components/toolbox/misc/Minimizable.tsx
+++ web/src/components/toolbox/misc/Minimizable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BsDashSquareFill, BsDashSquare } from 'react-icons/bs'
 import { motion, useSpring, useTransform } from 'framer-motion'
 
@@ -12,22 +12,58 @@ const Minimizable = ({
   children
 }: IMinimizable) => {
 
+  const containerRef = useRef<HTMLDivElement>(null)
   const [minimized, setMinimized] = useState<boolean>(false)
+
+  const hideSpring = useSpring(0)
   const minimizeSpring = useSpring(0)
-  const minimizeTransform = useTransform(minimizeSpring, [0, 1], [0, 1])
-  const displayTransform = useTransform(minimizeSpring, (x) => {
-    if (x === 0) return 'none'
-    else return 'block'
+
+  const opacityTransform = useTransform(hideSpring, [0, 1], [0, 1])
+  // const displayTransform = useTransform(minimizeSpring, (x) => {
+  //   if (x === 0) return 'none'
+  //   else return 'block'
+  // })
+  const offsetTransform = useTransform(minimizeSpring, (x) => {
+
+    if (containerRef.current == null) return 0;
+
+    let height_: number = (containerRef.current! as any).offsetHeight
+    return -1  * (1-x) * height_
   })
 
   useEffect(() => {
-    if (minimized) minimizeSpring.set(0)
+
+    const unsubHide = hideSpring.onChange((val: number) => {
+      if (val == 0) {
+        minimizeSpring.set(0)
+      }
+    })
+    const unsubMinimize = minimizeSpring.onChange((val: number) => {
+      if (val == 1) {
+        hideSpring.set(1)
+      }
+    })
+
+    return () => {
+
+      // unsubscribe the updater
+      unsubHide();
+      // unsubMinimize();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (minimized) hideSpring.set(0)
     else minimizeSpring.set(1)
-  }, [minimized, minimizeSpring])
+  }, [minimized, hideSpring])
 
   return (<div className="minimizable">
 
-    <div className="content-title" onClick={() => { setMinimized(!minimized) }}>
+    <div className="content-title" onClick={() => { setMinimized(!minimized) }} 
+      style={{
+        position: 'relative',
+        zIndex: 2
+      }}>
       <div className="icon-area">
         {minimized && <BsDashSquare />}
         {!minimized && <BsDashSquareFill />}
@@ -35,14 +71,18 @@ const Minimizable = ({
       <div className="title-text">{title}</div>
     </div>
 
-    <motion.div 
+    <motion.div
+      ref={containerRef}
       style={{
-        opacity: minimizeTransform,
-        display: displayTransform
+        zIndex: 1,
+        opacity: opacityTransform,
+        // display: displayTransform
+        marginTop: offsetTransform
       }}
       className="content-area">
       {children}
     </motion.div>
+
   </div>)
 }
 
