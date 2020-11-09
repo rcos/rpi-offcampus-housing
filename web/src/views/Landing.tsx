9baece3567c98ad9125b40a6fe910f17a492c66d
Abdul <muiz.rpi@gmail.com>
2020-11-09 03:41:16 -0500

    Added server config

--- web/src/views/Landing.tsx
+++ web/src/views/Landing.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useHistory} from 'react-router-dom'
 
 import LeftAndRight from '../components/toolbox/layout/LeftAndRight'
@@ -6,13 +6,36 @@ import Logo from '../components/Logo'
 import Button from '../components/toolbox/form/Button'
 import {BiLogIn} from 'react-icons/bi'
 import Centered from '../components/toolbox/layout/Centered'
+import {useMediaQuery} from 'react-responsive'
 
 const SearchView = () => {
 
+  const [centeredWidth, setCenteredWidth] = useState<number>(1400)
+
+  const calcCenterdWidth = (win_width: number) => {
+    if (win_width > 1400) setCenteredWidth(1400)
+    else if (win_width > 1200) setCenteredWidth(1200)
+    else if (win_width > 1000) setCenteredWidth(1000)
+    else if (win_width > 800) setCenteredWidth(800)
+  }
+
+  useEffect(() => {
+    calcCenterdWidth(window.innerWidth)
+    const resizeFn = (e: any) => {
+      let w = e.target.innerWidth
+      calcCenterdWidth(w)
+    }
+
+    window.addEventListener('resize', resizeFn)
+
+    return () => {
+      window.removeEventListener('resize', resizeFn)
+    }
+  }, [])
 
   return (<div>
     
-    <Centered width={1400} height="100%">
+    <Centered width={centeredWidth} height="100%">
       <React.Fragment>
 
         <div style={{marginTop: `20px`}}></div>
