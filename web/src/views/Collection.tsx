398564513cc143587c29cca5433a87be0f722705
Abdul <muiz.rpi@gmail.com>
2020-11-10 22:16:58 -0500

    Created collection view placeholder

--- web/src/views/Collection.tsx
+++ web/src/views/Collection.tsx
@@ -1,20 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import ViewWrapper from '../components/ViewWrapper'
 import {BiCollection} from 'react-icons/bi'
+import Pagination from '../components/toolbox/layout/Pagination'
+import Button from '../components/toolbox/form/Button'
+import {BiRightArrowAlt} from 'react-icons/bi'
+import {BsThreeDotsVertical} from 'react-icons/bs'
 
 const CollectionView = () => {
 
   // Container ref: the ref references the collection view, excluding the header
   const headerRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState<number>(0)
 
   useEffect(() => {
     const setContainerHeight = (h: number): void => {
       if (containerRef.current == null) return
-      // containerRef.current.style.height = `${h}px`
-      setHeight(h)
+      containerRef.current.style.height = `${h}px`
     }
 
     const calcualteContainerHeight = () => {
@@ -42,7 +44,7 @@ const CollectionView = () => {
   }, [])
 
   return (<ViewWrapper>
-    <div>
+    <div className="collection-holder">
 
       {/* Collection Header */}
       <div className="section-header left-and-right" ref={headerRef}>
@@ -51,12 +53,78 @@ const CollectionView = () => {
       </div>
 
       {/* Collection Entries Container */}
-      <div className="collection-container" ref={containerRef} style={{height: `${height}px`}}>
-        X
+      <div className="collection-container" ref={containerRef}>
+
+        <div className="collection-grid">
+          {Array.from(new Array(15), (x, i) => {
+            return <CollectionEntry key={i} />
+          })}
+        </div>
       </div>
 
+      {/* Pagination Area */}
+      <div className="pageination-holder">
+          <Pagination 
+            page={0}
+            pageChange={() => {}}
+            page_range={{min: 0, max: 5}}
+          />
+        </div>
     </div>
   </ViewWrapper>)
+}
+
+const CollectionEntry = () => {
+
+  return (<div className="collection-entry">
+    <div className="image-holder">
+      <div className="primary-image"></div>
+      <div className="secondary-images">
+        <div className="img-holder"></div>
+        <div className="img-holder"></div>
+      </div>
+    </div>
+    <div className="property-desc">
+      
+      {/* Property Header */}
+      <div>
+        <span style={{fontWeight: 600, fontSize: '0.9rem'}}>Sample Property Address Goes Here</span>
+      </div>
+
+      {/* Info goes here */}
+      <div className="kv-pair">
+        <div className="key">Date Added</div>
+        <div className="value">11/10/2020</div>
+      </div>
+      
+      <div className="kv-pair">
+        <div className="key">Landlord</div>
+        <div className="value">Jimmy Joe</div>
+      </div>
+    </div>
+
+    {/* Top Right Buttons */}
+    <div style={{
+      position: 'absolute',
+      top: 0, right: '10px'
+    }}>
+      <div className="icon-button"><BsThreeDotsVertical /></div>
+    </div>
+
+    {/* Bottom Right Buttons */}
+    <div style={{
+      position: 'absolute',
+      right: '10px',
+      bottom: 0
+    }}>
+      <Button 
+        text="View"
+        icon={<BiRightArrowAlt />}
+        iconLocation="right"
+        background="#99E1D9"
+      />
+    </div>
+  </div>)
 }
 
 export default CollectionView
