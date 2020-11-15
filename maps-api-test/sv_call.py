0cf80033e51d8449a62cf43c5ef4fec207e2a045
Josh Malek <17jmalek98@gmail.com>
2020-11-14 23:34:41 -0500

    changed coordinates for posterity

--- maps-api-test/sv_call.py
+++ maps-api-test/sv_call.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+# https://rrwen.github.io/google_streetview/
 # Define parameters for street view api
 API_KEY = os.getenv("STREET_VIEW_API_KEY")
 params = [{
 	'size': '600x300', # max 640x640 pixels
-	'location': '46.414382,10.013988',
+	'location': '42.728886,-73.677793',
 	'heading': '151.78',
 	'pitch': '-0.76',
 	'key': API_KEY
