b65bcfdf86b491a2c28a52836d1bb125cabeb520
Abdul <muiz.rpi@gmail.com>
2020-11-15 20:01:51 -0500

    Created instituion model

---
+++ server/GQL/entities/Institution.ts
@@ -0,0 +1,42 @@
+import { prop, getModelForClass } from "@typegoose/typegoose"
+import { Field, ObjectType, InputType, ID } from "type-graphql";
+import {APIResult} from "."
+
+@ObjectType({description: "Institution Location"})
+class InstitutionLocationInfo {
+  @Field()
+  @prop()
+  address: string;
+  
+  @Field()
+  @prop()
+  city: string;
+  
+  @Field()
+  @prop()
+  state: string;
+  
+  @Field()
+  @prop()
+  zip: string;
+}
+
+@ObjectType({description: "Institution Model"})
+export class Institution {
+
+  @Field(() => ID)
+  _id: string;
+
+  @Field()
+  @prop()
+  name: string;
+
+  @Field()
+  @prop()
+  location: InstitutionLocationInfo;
+}
+
+@ObjectType()
+export class InstitutionAPIResponse extends APIResult(Institution) {}
+
+export const InstitutionModel = getModelForClass(Institution)
