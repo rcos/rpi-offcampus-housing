31c35972f6c452dcf964d161f2dfeb88884fdbb5
Abdul <muiz.rpi@gmail.com>
2020-12-08 02:29:45 -0500

    Added confirm-code view for phone verification

--- web/src/views/PhoneVerifyView.tsx
+++ web/src/views/PhoneVerifyView.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FiAlertTriangle} from 'react-icons/fi'
+import {HiX} from 'react-icons/hi'
 
 import Centered from '../components/toolbox/layout/Centered'
 import Input, {numbersOnly, maxLen, $and} from '../components/toolbox/form/Input'
@@ -15,19 +16,101 @@ import Button from '../components/toolbox/form/Button'
 
 const PhoneVerifyView = () => {
 
+    const [onTwilioPortion, setOnTwilioPortion] = useState<boolean>(true)
+    const [phoneNumber, setPhoneNumber] = useState<{country_code: string, phone_number: string}>({
+        country_code: "+1",
+        phone_number: "1920194812"
+    })
+
     return (<Centered width={400} height={500}>
         <div>
-        <div className="title-1" style={{marginBottom: '20px'}}>Phone Number Verification</div>
-            <div className="error-line warning" style={{
-                position: 'relative',
-                top: '-10px'
-            }}>
-                <span className="icon-holder"><FiAlertTriangle /></span>
-                You must verify your phone number in order to continue.
+            <div className="title-1" style={{marginBottom: '20px'}}>Phone Number Verification</div>
+            
+            {onTwilioPortion && 
+            <div>
+
+                <div style={{
+                    fontFamily: 'mukta',
+                    fontSize: '0.9rem',
+                    lineHeight: '18px'
+                }}>A 6-number code has been sent to the phone number:</div>
+                <div style={{
+                    fontFamily: 'mukta',
+                    fontSize: `1rem`,
+                    marginTop: `5px`,
+                    position: 'relative'
+                }}>{phoneNumber.country_code} {phoneNumber.phone_number}
+                    <div className="subtle-button" style={{
+                        float: 'right',
+                        position: 'relative',
+                        top: '3px'
+                    }}>
+                        new number
+                    </div>
+                </div>
+
+                {/* Confirmation Code */}
+                <div style={{
+                    marginTop: `20px`
+                }}>
+                    <Input 
+                        label="confirm code"
+                        inputFilters={[numbersOnly]}
+                        autoFocus={true}
+                    />
+                    <div className="error-line error"><span className="icon-holder">
+                        <HiX /></span>Invalid code
+                    </div>
+                    
+                    <div style={{
+                        width: '100px',
+                        float: 'right'
+                    }}>
+                        <Button 
+                            text="Verify"
+                            background="#99E1D9"
+                        />
+                    </div>
+                    <div style={{
+                        width: '100px',
+                        float: 'right',
+                        marginRight: '10px'
+                    }}>
+                        <Button 
+                            text="Resend"
+                            background="#E4E4E4"
+                        />
+                    </div>
+
+                    <div style={{
+                        width: '100px',
+                        float: 'left',
+                        marginRight: '10px'
+                    }}>
+                        <Button 
+                            text="Cancel"
+                            background="#E4E4E4"
+                        />
+                    </div>
+                </div>
+
+            </div>}
+
+            {!onTwilioPortion && <div>
+                <div className="error-line warning" style={{
+                    position: 'relative',
+                    top: '-10px'
+                }}>
+
+                    <div>
+                        <span className="icon-holder"><FiAlertTriangle /></span>
+                        You must verify your phone number in order to continue.
+                    </div>
+                </div>
 
                 {/* Input Area */}
                 <div style={{display: 'flex', marginTop: `10px`}}>
-                    <div style={{width: `108px`, minWidth: `108px`, marginRight: `10px`}}>
+                    <div style={{width: `115px`, minWidth: `115px`, marginRight: `10px`}}>
                         <Input 
                             initial="+1"
                             label="Country Code"
@@ -50,8 +133,11 @@ const PhoneVerifyView = () => {
                         background="#99E1D9"
                     />
                 </div>
-            </div>
+            </div>}
+
         </div>
+
+            
     </Centered>)
 }
 
