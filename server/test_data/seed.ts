1319dec273b916189176bf01d807164e5f80738e
Abdul <muiz.rpi@gmail.com>
2020-12-09 07:01:01 -0500

    Updated seed to use gql schema

--- server/test_data/seed.ts
+++ server/test_data/seed.ts
@@ -5,6 +5,7 @@ import { promisify } from "util";
 import faker from "faker";
 
 import { ObjectID } from "mongodb";
+import mongoose from 'mongoose';
 import bcrypt from "bcrypt";
 
 import { ILandlord } from "../schemas/landlord.schema";
@@ -27,8 +28,144 @@ const DEFAULT_NUM_STUDENT_REVIEWS = 50;
 const DEFAULT_SEED = 1;
 
 // helper models
+import {Student as _Student_} from '../GQL/entities/Student'
+import {Landlord as _Landlord_} from '../GQL/entities/Landlord'
+
+// type MongoObjectID = {
+//   $oid: ObjectID;
+// };
+type Student = Omit<_Student_, "_id"> & {
+  _id: mongoose.Types.ObjectId;
+};
+
+class OIDFactory {
+  static id_memory: {[key: string]: mongoose.Types.ObjectId[]} = {};
+
+  /**
+   * Generate an object id that is unique to the collection_name
+   * @param collection_name: string => The collection_name to instantiate the
+   * ObjectId within.
+   */
+  static generateObjectID (collection_name: string): mongoose.Types.ObjectId {
+    if (!Object.prototype.hasOwnProperty.call(this.id_memory, collection_name)) this.id_memory[collection_name] = []
+    let oid: mongoose.Types.ObjectId;
+    do {
+      oid = mongoose.Types.ObjectId();
+    } while (this.id_memory[collection_name].includes( oid ))
+    this.id_memory[collection_name].push(oid)
+    return oid;
+  }
+}
+
+const objectFactory = <T>(n: number, generator: () => T) => {
+  const objects: T[] = [];
+  for (let i = 0; i < n; i++) {
+    objects.push(generator());
+  }
+  return objects;
+};
+
+/**
+ * generateStudent
+ * @desc Create a new student object with mock data
+ */
+const generateStudent = (): Student => {
+  let _fname: string = faker.name.firstName();
+  let _lname: string = faker.name.lastName();
+  let mock_student: Student = {
+    _id: OIDFactory.generateObjectID('student'),
+    first_name: _fname,
+    last_name: _lname,
+    email: faker.internet.email(_fname, _lname),
+    phone_number: faker.phone.phoneNumber("+1##########"),
+    saved_collection: [],
+    auth_info: {
+      institution_id: '',
+      cas_id: `${_fname.replace(' ', '_')}_${_lname.replace(' ', '_')}`
+    }
+  }
+  return mock_student;
+}
+const generateStudents = (n: number) => objectFactory(n, generateStudent);
+
+/////////////////////////////////////////////////////////
+
+type GenerateDataProps = {
+  numStudents: number;
+} & Partial<SeedProps>;
+const generateData = ({
+  seed,
+  numStudents
+}: GenerateDataProps) => {
+  if (seed !== undefined) {
+    seedGenerator({ seed });
+  }
+
+  const students = generateStudents(numStudents);
+
+  return {
+    students
+  };
+};
+
+interface SeedProps {
+  seed: number;
+}
+const seedGenerator = ({ seed }: SeedProps) => {
+  faker.seed(seed);
+};
+seedGenerator({ seed: DEFAULT_SEED });
+
+const writeFileAsync = promisify(fs.writeFile);
+type WriteDataProps = {
+  directory?: string;
+} & GenerateDataProps;
+const writeData = async (props?: WriteDataProps & Partial<SeedProps>) => {
+  const { seed, directory, ...generateDataProps } = props ?? {};
+
+  if (seed !== undefined) {
+    seedGenerator({ seed });
+  }
+
+  const directoryPath = path.resolve(
+    __dirname,
+    directory ?? DEFAULT_OUT_DIRECTORY
+  );
+
+  const { students } = generateData({
+    numStudents: DEFAULT_NUM_STUDENTS
+    ...generateDataProps,
+  });
+
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath);
+  }
+
+  return Promise.all([
+    writeFileAsync(
+      path.resolve(directoryPath, DEFAULT_OUT_STUDENTS_FILE),
+      JSON.stringify(students)
+    )
+    /*
+    writeFileAsync(
+      path.resolve(directoryPath, DEFAULT_OUT_PROPERTIES_FILE),
+      JSON.stringify(properties)
+    ),
+    writeFileAsync(
+      path.resolve(directoryPath, DEFAULT_OUT_LANDLORDS_FILE),
+      JSON.stringify(landlords)
+    ),
+    writeFileAsync(
+      path.resolve(directoryPath, DEFAULT_OUT_STUDENT_REVIEWS),
+      JSON.stringify(studentReviews)
+    ),
+    */
+  ]);
+};
+
 
 // need to do this way so stringify is valid
+/*
 type MongoObjectID = {
   $oid: ObjectID;
 };
@@ -59,7 +196,7 @@ const objectFactory = <T>(n: number, generator: () => T) => {
 
 const writeFileAsync = promisify(fs.writeFile);
 
-/** Return random rating 1-5 inclusive */
+// Return random rating 1-5 inclusive
 const getRandomRating = () => faker.random.number({ min: 1, max: 5 });
 
 const getRandomTerm = () => {
@@ -339,3 +476,4 @@ export {
   generateData,
   writeData,
 };
+*/
