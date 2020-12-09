6efe08b529202818e7bf3411856d28d18a15b7e9
Abdul <muiz.rpi@gmail.com>
2020-12-09 03:03:52 -0500

    wrapped twilio initializer

--- server/vendors/Twilio.ts
+++ server/vendors/Twilio.ts
@@ -1,4 +1,10 @@
-const twilioClient = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN)
+
+import chalk from 'chalk'
+
+let twilioClient: null | any = null;
+if (process.env.TWILIO_SID && process.env.TWILIO_AUTH_TOKEN) {
+    twilioClient = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+}
 /**
  * Twilio API Wrapper
  * 
@@ -8,6 +14,10 @@ const twilioClient = require('twilio')(process.env.TWILIO_SID, process.env.TWILI
 export const Twilio = {
 
     sendSMS: (message: string, to: string) => {
+        if (twilioClient == null) {
+            console.log(chalk.bgRed(`❌ Error: Twilio client not initialized.`))
+            return;
+        }
         twilioClient.messages.create({
             body: message,
             from: '+12055510725',
