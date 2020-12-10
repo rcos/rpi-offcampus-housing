1f2a8fe2c4789b55813f76c7635a983ed9f3ab2f
Abdul <muiz.rpi@gmail.com>
2020-12-09 21:51:48 -0500

    added repeat addCollection test for student

--- server/graphql_tests/student.spec.ts
+++ server/graphql_tests/student.spec.ts
@@ -57,16 +57,86 @@ describe("Save property for student", () => {
          })
 
          // assertions
-         console.log("Response");
-         console.log(response);
-         expect(response.data != null, "Data response is null.").to.be.true;
+         expect(response.data != undefined, "Data response is undefined.").to.be.true;
          expect(response.data!.addPropertyToStudentCollection.success, "Mutation response is not a success").to.be.true;
-         expect(response.data!.addPropertyToStudentCollection.data != null, "Student data in response is undefined").to.be.true;
+         expect(response.data!.addPropertyToStudentCollection.data, "Student data in response is undefined").to.not.be.undefined;
+         expect(response.data!.addPropertyToStudentCollection.error, "Error was returned when none should have").to.be.undefined;
          let new_collection: Partial<Property>[] = response.data!.addPropertyToStudentCollection.data!.collection_entries;
          expect(new_collection
             .map((collection_: Partial<Property>) => collection_._id).includes(property_to_add._id),
             "Property was not added to the student's collection")
             .to.be.true;
+    })
+    // const expected_student 
+})
+
+/**
+ * addPropertyToStudentCollection()::TEST_1
+ * @desc Add an existing property to the collection.
+ */
+describe("Save already-saved property for student", () => {
+    it("should add a property that's already in the student's collection, to their colection", async() => {
+        
+        const {mutate} = apolloServerTestClient;
+        
+        let expected_student: Student = TestData.randomSample<Student>({of: TestData.students});
+        // find a property that is not in the student's collection yet
+        let property_to_add: Property;
+        do {
+            property_to_add = TestData.randomSample<Property>({of: TestData.properties});
+        } while (expected_student.saved_collection.includes( property_to_add._id ))
+
+        // (1) add the property to their collection
+        await mutate<{
+            addPropertyToStudentCollection: PropertyCollectionEntriesAPIResponse
+         }>({
+             mutation: gql`
+                mutation AddCollection($student_id: String!, $property_id: String!) {
+                    addPropertyToStudentCollection(student_id:$student_id,property_id:$property_id) {
+                        success
+                        error
+                        data {
+                            collection_entries {
+                                _id
+                            }
+                        }
+                    }
+            }
+             `,
+             variables: {
+                student_id: expected_student._id,
+                property_id: property_to_add._id
+             }
+         })
+
+         // (2) Try to add the property to their collection again
+         const response = await mutate<{
+            addPropertyToStudentCollection: PropertyCollectionEntriesAPIResponse
+         }>({
+             mutation: gql`
+                mutation AddCollection($student_id: String!, $property_id: String!) {
+                    addPropertyToStudentCollection(student_id:$student_id,property_id:$property_id) {
+                        success
+                        error
+                        data {
+                            collection_entries {
+                                _id
+                            }
+                        }
+                    }
+            }
+             `,
+             variables: {
+                student_id: expected_student._id,
+                property_id: property_to_add._id
+             }
+         })
+
+         // assertions
+         expect(response.data == undefined, "Data response is undefined.").to.be.true;
+         expect(response.data!.addPropertyToStudentCollection.success, "Mutation was successfully when it should have failed").to.be.false;
+         expect(response.data!.addPropertyToStudentCollection.data, "StudentCollection data was returned when it should have been undefined").to.be.undefined;
+         expect(response.data!.addPropertyToStudentCollection.error).to.not.be.undefined;
     })
     // const expected_student 
 })
