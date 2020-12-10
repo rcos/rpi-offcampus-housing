8f040a30cd5b759472e2efed11390ecdae8928f3
Abdul <muiz.rpi@gmail.com>
2020-12-10 04:24:11 -0500

    replaced old menu with new collapsable menu:

--- web/src/components/ViewWrapper.tsx
+++ web/src/components/ViewWrapper.tsx
@@ -1,17 +1,21 @@
 import React, {useState, useEffect, useRef, ChangeEvent} from 'react'
+import {useSpring, useTransform, motion} from 'framer-motion'
 import Centered from './toolbox/layout/Centered'
 
 import Navbar from './AuthNavbar'
 import LandlordNavbar from './LandlordAuthNavbar'
 import AuthAPI from '../API/AuthAPI'
 
+import Logo from '../components/Logo'
 import Button from '../components/toolbox/form/Button'
 import {RiCheckLine, RiBugLine} from 'react-icons/ri'
 import {usePopup} from '../components/hooks/usePopupHook'
-import { HiOutlineNewspaper, HiCheckCircle, HiTerminal, HiOutlineHome, HiLogout, HiClipboard, HiOutlineChatAlt } from 'react-icons/hi';
+import { HiOutlineNewspaper, HiCheckCircle, HiTerminal, 
+  HiOutlineHome, HiLogout, HiClipboard, HiOutlineChatAlt,
+  HiOutlineChevronLeft } from 'react-icons/hi';
 import { FiFileText } from 'react-icons/fi';
 import { BiSearch, BiCollection } from "react-icons/bi";
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import {useMediaQuery} from 'react-responsive';
 import {useSelector} from 'react-redux'
 import {ReduxState} from '../redux/reducers/all_reducers'
@@ -41,6 +45,7 @@ const ViewWrapper = ({children, showNavbar}: {children: any, showNavbar?: boolea
   // const [viewWidth, setViewWidth] = useState<number>(1400)
   const [navbarMinMode, setNavbarMinMode] = useState<boolean>(false)
   const [feedbackSubmitted, setFeedbackSubmitted] = useState<boolean>(false)
+  const [menuCollapsed, setMenuCollapsed] = useState<boolean>(false)
 
   const setHeight = () => {
     // set the height
@@ -307,65 +312,145 @@ const ViewWrapper = ({children, showNavbar}: {children: any, showNavbar?: boolea
     </div>)
   })
 
-  return (<Centered height="100%" horizontalBuffer={isTablet? 150 : 400}>
-    <React.Fragment>
-      <div>
-        {user && user.type && user.type == "student" && <Navbar showNavbar={showNavbar} />}
-        {user && user.type && user.type == "landlord" && <LandlordNavbar showNavbar={showNavbar} />}
+  // collapsed menu logic
+  const menuCollapseInitSpring = useSpring(menuCollapsed ? 0 : 1)
+  const menuLabelHeightTransform = useTransform(menuCollapseInitSpring, (x: number) => `${x * 30}px`)
+
+  const menuCollapseIntermediate = useSpring(menuCollapsed ? 0 : 1)
+  const menuWidthCollapseTransform = useTransform(menuCollapseIntermediate, [0, 1], [60, 200])
+
+  useEffect(() => {
+    if (menuCollapsed) {
+      menuCollapseInitSpring.set(0)
+    }
+    else {
+      menuCollapseIntermediate.set(1)
+    }
+
+    let unsubMotionCollapseInitSpring = menuCollapseInitSpring.onChange((x: number) => {
+      if (x == 0) menuCollapseIntermediate.set(0)
+    })
+
+    let unsubMenuCollapseIntermediate = menuCollapseIntermediate.onChange((x: number) => {
+      if (x == 1) menuCollapseInitSpring.set(1)
+    })
+
+    return () => {
+      unsubMotionCollapseInitSpring()
+      unsubMenuCollapseIntermediate()
+    }
+  }, [menuCollapsed])
+
+  return (<React.Fragment>
+
+    <motion.div className={`vertical-navbar ${menuCollapsed ? 'collapsed': ''}`} style={{
+      width: menuWidthCollapseTransform
+    }}>
+
+      <div className="logo-line">
+        <div className="logo-area"><div className="app-logo" /></div>
+        <motion.div className="logo-text" style={{opacity: menuCollapseInitSpring}}>offcmpus</motion.div>
+        <div className={`menu-collapse-btn ${menuCollapsed? 'collapsed' : ''}`} onClick={() => setMenuCollapsed(!menuCollapsed)}><HiOutlineChevronLeft /></div>
       </div>
-      <div className="app-view-area" ref={containerRef}>
-        {showNavbar != false && <div className="user-navbar">
 
-          {Object.keys(pageLinks).map((page_: string, index: number) => {
+      {/* Menu Area */}
+      <motion.div className="menu-label" style={{
+        opacity: menuCollapseInitSpring,
+        height: menuLabelHeightTransform
+      }}>menu</motion.div>
+      <div className={`collapse-separator ${menuCollapsed ? 'collapsed' : ''}`} />
 
-              return (<div key={index} style={{marginBottom: '10px'}} onClick={() => history.push((pageLinks as any)[page_].target)}>
-                <div className={`icon-link ${window.location.pathname.toLowerCase() === (pageLinks as any)[page_].target.toLowerCase() ? 'active' : ''}`}>
-                  <div className="icon-holder">{(pageLinks as any)[page_].icon}</div>
-                  <div className="link-desc">{(pageLinks as any)[page_].name}</div>
-                </div>
-              </div>)
+      {Object.keys(pageLinks).map((page_: any, index: number) => 
+      (<Link to={pageLinks[page_].target} key={index}>
+        <div className={`menu-link ${window.location.pathname.toLowerCase() === pageLinks[page_].target.toLowerCase() ? 'active' : ''}`}>
+          <div className={`icon ${menuCollapsed ? 'collapsed' : ''}`}>{pageLinks[page_].icon}</div>
+          <motion.div className="text" style={{opacity: menuCollapseInitSpring}}>{pageLinks[page_].name}</motion.div>
+      </div>
+      </Link>))}
 
-          })}
-
-          {isOwnershipReviewer() &&
-            <div>
-              <div className="submenu-title">moderator</div>
-              {Object.keys(reviewerLinks).map((page_: string, index: number) => {
-
-              return (<div key={index} style={{marginBottom: '10px'}} onClick={() => history.push((reviewerLinks as any)[page_].target)}>
-                <div className={`icon-link ${window.location.pathname.toLowerCase() === (reviewerLinks as any)[page_].target.toLowerCase() ? 'active' : ''}`}>
-                  <div className="icon-holder">{(reviewerLinks as any)[page_].icon}</div>
-                  <div className="link-desc">{(reviewerLinks as any)[page_].name}</div>
-                </div>
-              </div>)
-
-              })}
-            </div>
-          }
-
-          {/* Footer Buttons */}
-          <div className="bottom-area">
-            {/* Feedback */}
-            <div className="icon-link" onClick={initFeedback}>
-              <div className="icon-holder"><HiOutlineChatAlt /></div>
-              <div className="link-desc">Feedback</div>
-            </div>
-            
-            {/* Logout */}
-            <div className="icon-link" onClick={logout}>
-              <div className="icon-holder"><HiLogout /></div>
-              <div className="link-desc">Logout</div>
-            </div>
+    {isOwnershipReviewer() &&
+      <React.Fragment>
+        <motion.div className="menu-label" style={{
+          opacity: menuCollapseInitSpring,
+          height: menuLabelHeightTransform
+        }}>moderator</motion.div>
+        <div className={`collapse-separator ${menuCollapsed ? 'collapsed' : ''}`} />
+      
+        {Object.keys(reviewerLinks).map((page_: any, index: number) => 
+        (<Link to={(reviewerLinks as any)[page_].target} key={index}>
+          <div className={`menu-link ${window.location.pathname.toLowerCase() === (reviewerLinks as any)[page_].target.toLowerCase() ? 'active' : ''}`}>
+            <div className={`icon ${menuCollapsed ? 'collapsed' : ''}`}>{(reviewerLinks as any)[page_].icon}</div>
+            <motion.div className="text" style={{opacity: menuCollapseInitSpring}}>{(reviewerLinks as any)[page_].name}</motion.div>
           </div>
+        </Link>))}
+      </React.Fragment>
+    }
 
+    </motion.div>
 
+    <Centered height="100%" horizontalBuffer={isTablet? 150 : 600}>
+      <React.Fragment>
+        <div style={{marginTop: '60px'}}></div>
+        {false && <div style={{border: `1px solid black`}}>
+          {/* {user && user.type && user.type == "student" && <Navbar showNavbar={showNavbar} />}
+          {user && user.type && user.type == "landlord" && <LandlordNavbar showNavbar={showNavbar} />} */}
         </div>}
-        <div className="content-area">
-          {children}
+        <div className="app-view-area" ref={containerRef}>
+          {false && showNavbar != false && <div className="user-navbar">
+
+            {Object.keys(pageLinks).map((page_: string, index: number) => {
+
+                return (<div key={index} style={{marginBottom: '10px'}} onClick={() => history.push((pageLinks as any)[page_].target)}>
+                  <div className={`icon-link ${window.location.pathname.toLowerCase() === (pageLinks as any)[page_].target.toLowerCase() ? 'active' : ''}`}>
+                    <div className="icon-holder">{(pageLinks as any)[page_].icon}</div>
+                    <div className="link-desc">{(pageLinks as any)[page_].name}</div>
+                  </div>
+                </div>)
+
+            })}
+
+            {isOwnershipReviewer() &&
+              <div>
+                <div className="submenu-title">moderator</div>
+                {Object.keys(reviewerLinks).map((page_: string, index: number) => {
+
+                return (<div key={index} style={{marginBottom: '10px'}} onClick={() => history.push((reviewerLinks as any)[page_].target)}>
+                  <div className={`icon-link ${window.location.pathname.toLowerCase() === (reviewerLinks as any)[page_].target.toLowerCase() ? 'active' : ''}`}>
+                    <div className="icon-holder">{(reviewerLinks as any)[page_].icon}</div>
+                    <div className="link-desc">{(reviewerLinks as any)[page_].name}</div>
+                  </div>
+                </div>)
+
+                })}
+              </div>
+            }
+
+            {/* Footer Buttons */}
+            <div className="bottom-area">
+              {/* Feedback */}
+              <div className="icon-link" onClick={initFeedback}>
+                <div className="icon-holder"><HiOutlineChatAlt /></div>
+                <div className="link-desc">Feedback</div>
+              </div>
+              
+              {/* Logout */}
+              <div className="icon-link" onClick={logout}>
+                <div className="icon-holder"><HiLogout /></div>
+                <div className="link-desc">Logout</div>
+              </div>
+            </div>
+
+
+          </div>}
+          <div className="content-area">
+            {children}
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  </Centered>)
+      </React.Fragment>
+    </Centered>
+
+
+  </React.Fragment>)
 }
 
 export default ViewWrapper
